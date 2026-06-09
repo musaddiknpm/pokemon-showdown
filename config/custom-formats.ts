8bf21dea4eb9b851fae0f9507ef2a0317fdf5048
Musaddik Temkar <musaddiknpm@gmail.com>
2026-06-09 17:11:41 +0530

    Add impulse custom formats

---
+++ config/custom-formats.ts
@@ -0,0 +1,32 @@
+export const Formats: FormatList = [
+        {
+                section: "PokèRogue Mod",
+        },
+        {
+                name: "[Gen 9] PokeRogue Classic",
+                mod: 'pokerogue',
+                ruleset: [
+                        'Cancel Mod',
+                        'HP Percentage Mod',
+                        'PokeRogue Classic',
+                        'PokeRogue EXP Tracker',
+                ],
+                DefaultLevel: 1,
+                MaxLevel: 999999,
+                banlist: [],
+        },
+        {
+                name: "[Gen 9] PokeRogue Classic Doubles",
+                mod: 'pokerogue',
+    gameType: 'doubles',
+                ruleset: [
+                        'Cancel Mod',
+                        'HP Percentage Mod',
+                        'PokeRogue Classic',
+                        'PokeRogue EXP Tracker',
+                ],
+                DefaultLevel: 1,
+                MaxLevel: 999999,
+                banlist: [],
+        },
+];
